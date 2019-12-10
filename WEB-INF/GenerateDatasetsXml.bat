cf046442d9594eafe4e0101c5ef44e8e66b4401b
Bob Simons <bob.simons@noaa.gov>
2019-12-10 06:34:56 -0800

    merged Marco Alba's new marker types

--- WEB-INF/GenerateDatasetsXml.bat
+++ WEB-INF/GenerateDatasetsXml.bat
@@ -1,4 +1,4 @@
 rem This is the Windows batch file to run GenerateDatasetsXml.
 rem See http://coastwatch.pfeg.noaa.gov/erddap/download/setupDatasetsXml.html#Tools
 
-java -cp ./classes;../../../lib/servlet-api.jar;lib/cassandra-driver-core.jar;lib/netty-all.jar;lib/guava.jar;lib/metrics-core.jar;lib/lz4.jar;lib/snappy-java.jar;lib/commons-compress.jar;lib/itext-1.3.1.jar;lib/lucene-core.jar;lib/mail.jar;lib/netcdfAll-latest.jar;lib/slf4j.jar;lib/postgresql.jdbc.jar -Xms1000M -Xmx1000M  gov.noaa.pfel.erddap.GenerateDatasetsXml %*
+java -cp ./classes;../../../lib/servlet-api.jar;lib/cassandra-driver-core.jar;lib/netty-all.jar;lib/guava.jar;lib/metrics-core.jar;lib/lz4.jar;lib/snappy-java.jar;lib/commons-compress.jar;lib/commons-jexl.jar;lib/itext-1.3.1.jar;lib/lucene-core.jar;lib/mail.jar;lib/netcdfAll-latest.jar;lib/slf4j.jar;lib/postgresql.jdbc.jar -Xms1000M -Xmx1000M  gov.noaa.pfel.erddap.GenerateDatasetsXml %*
