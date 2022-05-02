0c8c7f1a542affb60d571a6f0db5eb62f4946b46
Chris John <Chris.P.John@gmail.com>
2022-05-02 10:38:42 -0700

    Add ThreadedWorkManager to encapsulate the threading logic
    Use the new class in EDDTableFromFiles and EDDGridFromFiles.
    
    Remove the threading logic from GridDataAccessor. I moved the layer that does threading to EDDGridFromFiles. Threading in GridDataAccessor uses significantly more memory. EDDGridFromFiles only has one file per chunk in EDDGridFromNcFiles.testBigRequestSpeed, but based on other tests, I believe live data sets are more likely to have multiple files per chunk:
    
    testNThreads: 151
    id: testGridNThreads
    
    testNccsv: 2
    id: testGriddedNcFiles
    
    testReplaceFromFileName: 8
    id: nceiPH53sstd1day
    
    Former-commit-id: 2964c1f15cd7d339618cb59ef3e335954500e01f [formerly 9e8c0e516c97f3f9f8ce8f32c753a25d1fc84475] [formerly 74ddc113f3d1925d5dc8affae1bc02e616a8f24f [formerly 86322374aa0bfa527bb2689a7344a729217b59c3]]
    Former-commit-id: 4a0f51d2e014c67dc89d466690da397010cdf39d [formerly d13a684b633202440ab536735b951c6e3a944b80]
    Former-commit-id: cd4cb121f0932f16e9e2843a15357daf6dc20535
    Former-commit-id: 90f5e77db194c15b94c9033224ff90abc3c4e008

---
+++ WEB-INF/classes/gov/noaa/pfel/erddap/util/ThreadedWorkManager.java
@@ -0,0 +1,67 @@
+package gov.noaa.pfel.erddap.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+
+public class ThreadedWorkManager<T> {
+    ExecutorService executorService = null;
+    List<FutureTask<T>> taskList = new ArrayList<>();
+    WorkConsumer<T> processor;
+    
+    int completed = 0;
+    
+    public ThreadedWorkManager (int nThreads, WorkConsumer<T> processResult) {
+        if (nThreads > 1) {
+            executorService = Executors.newFixedThreadPool(nThreads);
+        }
+        processor = processResult;
+    }
+    
+    public void addTask(Callable<T> callable) throws Exception, Throwable {
+        // If we're threaded add the work to the thread.
+        if (executorService != null) {
+            FutureTask<T> task = new FutureTask<T>(callable);
+            taskList.add(task);
+            if (executorService != null) {
+                executorService.submit(task);
+            }
+        } else {
+            // No threading here, just do the work and process it.
+            processor.accept(callable.call());
+        }
+    }
+    
+    public boolean hasNext() {
+        return taskList.size() > completed;
+    }
+    
+    public T getNextTaskResult() throws InterruptedException, ExecutionException {
+        //get results table from a futureTask
+        //Put null in that position in futureTasks so it can be gc'd after this method
+        FutureTask<T> task = taskList.set(completed++,  null);
+        return task.get();
+    }
+    
+    public void finishedEnqueing() {
+        if (executorService != null) {
+            executorService.shutdown();
+        }
+    }
+    
+    public void forceShutdown() {
+        if (executorService != null) {
+            executorService.shutdownNow();
+        }
+    }
+    
+    public void processResults() throws InterruptedException, ExecutionException, Throwable {
+        while(hasNext()) {
+            processor.accept(getNextTaskResult());
+        }
+    }
+}
