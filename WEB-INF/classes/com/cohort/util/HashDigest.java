18424e1ddabfc1e1cba55d2b2be2bb4252530d0a
Chris John <chris.john@noaa.gov>
2023-12-01 10:56:42 -0500

    Update HashDigest tests
    They now use JUnit and fixed the hashes since the file used to test the hashing algorithms has been changed.

--- WEB-INF/classes/com/cohort/util/HashDigest.java
+++ WEB-INF/classes/com/cohort/util/HashDigest.java
@@ -314,34 +314,6 @@ public class HashDigest {
 "where algorithm can be MD5, SHA-1, or SHA-256.\n";
 
     /**
-     * This tests this class.
-     */
-    public static void basicTest() throws Throwable {
-        System.out.println("*** HashDigest.basicTest");
-        String tName = String2.replaceAll(
-            File2.getClassPath() + "com\\cohort\\util\\License.txt", "\\", "/");
-        Test.ensureEqual(doIt(new String[]{"type:MD5"}), 
-            "Neither password or filename was specified.\n" + usage, "");
-        Test.ensureEqual(doIt(new String[]{"password:myPassword", "type:MD-5"}), 
-            "Invalid algorithm.\n" + usage, "");
-        Test.ensureEqual(doIt(new String[]{"password:myPassword", "type:MD5"}), 
-            "deb1536f480475f7d593219aa1afd74c", "");
-        Test.ensureEqual(doIt(new String[]{"password:myPassword", "type:SHA-1"}), 
-            "5413ee24723bba2c5a6ba2d0196c78b3ee4628d1", "");
-        Test.ensureEqual(doIt(new String[]{"password:myPassword", "type:SHA-256"}), 
-            "76549b827ec46e705fd03831813fa52172338f0dfcbd711ed44b81a96dac51c6", "");
-        Test.ensureEqual(doIt(new String[]{"filename:" + tName, "type:MD5"}), 
-            "327fbb2aa6c6297d4fdd5fdf4b14e289", "");
-        Test.ensureEqual(doIt(new String[]{"filename:" + tName, "type:SHA-256"}), 
-            "e376c88953b2d56b00783fed071f1875e8ed94230f4e14eee5bce8bd608de5e6", "");
-        Test.ensureEqual(doIt(new String[]{"filename:" + tName, "type:SHA-256", "-file"}), 
-            "Created " + tName + ".sha256", "");
-        Test.ensureEqual(File2.readFromFileUtf8(tName + ".sha256")[1],
-            "e376c88953b2d56b00783fed071f1875e8ed94230f4e14eee5bce8bd608de5e6  License.txt\n", "");
-        File2.delete(tName + ".sha256");
-    }
-
-    /**
      * This does the work.
      */
     public static String doIt(String args[]) throws Throwable {
@@ -398,7 +370,6 @@ public class HashDigest {
                     //if (test ==  0) ...;
 
                 } else {
-                    if (test ==  0) basicTest();
                 }
 
                 String2.log(msg + test + " finished successfully in " + (System.currentTimeMillis() - time) + " ms.");
