257c7bff04009355bcbaf728d92f86a34bd4db47
Chris John <chris.john@noaa.gov>
2024-01-04 12:42:51 -0500

    Clean up old test function RegexFilenameFilter

--- WEB-INF/classes/gov/noaa/pfel/coastwatch/util/RegexFilenameFilter.java
+++ WEB-INF/classes/gov/noaa/pfel/coastwatch/util/RegexFilenameFilter.java
@@ -8,12 +8,9 @@ import com.cohort.array.LongArray;
 import com.cohort.array.PrimitiveArray;
 import com.cohort.array.StringArray;
 
-import com.cohort.util.Calendar2;
-import com.cohort.util.Math2;
 import com.cohort.util.File2;
 import com.cohort.util.MustBe;
 import com.cohort.util.String2;
-import com.cohort.util.Test;
 
 import java.io.File;
 import java.io.FilenameFilter;
@@ -21,7 +18,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.regex.Pattern;
-import java.util.regex.Matcher;
 
 /**
  * A filter to find files (and directories!) whose names match a specified 
@@ -379,46 +375,5 @@ public class RegexFilenameFilter implements FilenameFilter {
             File2.touch(fullName);
         }
         return true;
-    }
-
-    /**
-     * This runs all of the interactive or not interactive tests for this class.
-     *
-     * @param errorSB all caught exceptions are logged to this.
-     * @param interactive  If true, this runs all of the interactive tests; 
-     *   otherwise, this runs all of the non-interactive tests.
-     * @param doSlowTestsToo If true, this runs the slow tests, too.
-     * @param firstTest The first test to be run (0...).  Test numbers may change.
-     * @param lastTest The last test to be run, inclusive (0..., or -1 for the last test). 
-     *   Test numbers may change.
-     */
-    public static void test(StringBuilder errorSB, boolean interactive, 
-        boolean doSlowTestsToo, int firstTest, int lastTest) {
-        if (lastTest < 0)
-            lastTest = interactive? -1 : 0;
-        String msg = "\n^^^ RegexFilenameFilter.test(" + interactive + ") test=";
-
-        for (int test = firstTest; test <= lastTest; test++) {
-            try {
-                long time = System.currentTimeMillis();
-                String2.log(msg + test);
-            
-                if (interactive) {
-                    //if (test ==  0) ...;
-
-                } else {
-                    if (test ==  0) basicTest();
-                }
-
-                String2.log(msg + test + " finished successfully in " + (System.currentTimeMillis() - time) + " ms.");
-            } catch (Throwable testThrowable) {
-                String eMsg = msg + test + " caught throwable:\n" + 
-                    MustBe.throwableToString(testThrowable);
-                errorSB.append(eMsg);
-                String2.log(eMsg);
-                if (interactive) 
-                    String2.pressEnterToContinue("");
-            }
-        }
     }
 }
