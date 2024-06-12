ef856c61f912330b2ed5f4859a8e47238c42e5fd
Ayush Singh <ayushsingh01042003@gmail.com>
2024-06-12 22:55:11 +0530

    Replace hardcoded strings with file paths

--- src/test/java/gov/noaa/pfel/erddap/LoadDatasetsTests.java
+++ src/test/java/gov/noaa/pfel/erddap/LoadDatasetsTests.java
@@ -1,13 +1,11 @@
 package gov.noaa.pfel.erddap;
 
-import com.cohort.util.String2;
+import com.cohort.util.File2;
 import gov.noaa.pfel.erddap.util.EDStatic;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import testDataset.Initialization;
-
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -20,27 +18,8 @@ public class LoadDatasetsTests {
 
     @Test
     void failedToLoadDatasetsTest() throws Throwable {
-        InputStream inputStream = new ByteArrayInputStream(String2.toByteArray(
-                "<?xml version=\"1.0\" encoding=\"ISO-8859-1\" ?>\n" +
-                        "<erddapDatasets>\n" +
-                        "<dataset type=\"EDDGridFromEtopo\" datasetID=\"etopo180\" />\n" +
-                        "<dataset type=\"EDDGridFromEtopo\" datasetID=\"etopo360\" />\n"+
-                        "</erddapDatasets>\n" +
-                        ""));
-
-        loadDatasets = new LoadDatasets(new Erddap(), EDStatic.datasetsRegex, inputStream, true);
-        loadDatasets.run();
-        assertEquals(EDStatic.datasetsThatFailedToLoad, "n Datasets Failed To Load (in the last major LoadDatasets) = 0\n");
-
-        inputStream = new ByteArrayInputStream(String2.toByteArray(
-                "<?xml version=\"1.0\" encoding=\"ISO-8859-1\" ?>\n" +
-                        "<erddapDatasets>\n" +
-                        "<dataset type=\"EDDGridFromEtopo\" datasetID=\"etopo180\" />\n" +
-                        "<dataset type=\"EDDGridFromEtopo\" datasetID=\"etopo36\" />\n"+ //Made a typo in the ID here so the dataset fails to load
-                        "</erddapDatasets>\n" +
-                        ""));
-
-        loadDatasets = new LoadDatasets(new Erddap(), EDStatic.datasetsRegex, inputStream, true);
+        String pathToDatasetsXml = Objects.requireNonNull(LoadDatasets.class.getResource("/datasets/failedToLoadDatasetsTest.xml")).getPath();
+        loadDatasets = new LoadDatasets(new Erddap(), EDStatic.datasetsRegex, File2.getBufferedInputStream(pathToDatasetsXml), true);
         loadDatasets.run();
         assertEquals(EDStatic.datasetsThatFailedToLoad, "n Datasets Failed To Load (in the last major LoadDatasets) = 1\n" +
                                                                "    etopo36, (end)\n");
@@ -48,33 +27,21 @@ public class LoadDatasetsTests {
 
     @Test
     void duplicateDatasetsTest() throws Throwable {
-        InputStream inputStream = new ByteArrayInputStream(String2.toByteArray(
-                "<?xml version=\"1.0\" encoding=\"ISO-8859-1\" ?>\n" +
-                        "<erddapDatasets>\n" +
-                        "<dataset type=\"EDDGridFromEtopo\" datasetID=\"etopo180\" />\n" +
-                        "<dataset type=\"EDDGridFromEtopo\" datasetID=\"etopo180\" />\n"+
-                        "</erddapDatasets>\n" +
-                        ""));
-
-        loadDatasets = new LoadDatasets(new Erddap(), EDStatic.datasetsRegex, inputStream, true);
+        String pathToDatasetsXml = Objects.requireNonNull(LoadDatasets.class.getResource("/datasets/duplicateDatasetsTest.xml")).getPath();
+        loadDatasets = new LoadDatasets(new Erddap(), EDStatic.datasetsRegex, File2.getBufferedInputStream(pathToDatasetsXml), true);
         loadDatasets.run();
         assertEquals(EDStatic.errorsDuringMajorReload, "ERROR: Duplicate datasetIDs in datasets.xml:\n" +
                                                               "    etopo180\n");
     }
 
-//    @Test
-//    void numberOfGridAndTableDatasetsTest() throws Throwable {
-//        InputStream inputStream = new ByteArrayInputStream(String2.toByteArray(
-//                "<?xml version=\"1.0\" encoding=\"ISO-8859-1\" ?>\n" +
-//                        "<erddapDatasets>\n" +
-//                        "</erddapDatasets>\n"
-//                        ));
-//
-//        loadDatasets = new LoadDatasets(new Erddap(), EDStatic.datasetsRegex, inputStream, true);
-//        loadDatasets.run();
-//        System.out.println(EDStatic.nTableDatasets);
-//        System.out.println(EDStatic.nGridDatasets);
-//        assertEquals(1 + 1, 2);
-//
-//    }
+    @Test
+    void numberOfGridAndTableDatasetsTest() throws Throwable {
+        String pathToDatasetsXml = Objects.requireNonNull(LoadDatasets.class.getResource("/datasets/numberOfGridAndTableDatasetsTest.xml")).getPath();
+        loadDatasets = new LoadDatasets(new Erddap(), EDStatic.datasetsRegex, File2.getBufferedInputStream(pathToDatasetsXml), true);
+        loadDatasets.run();
+        System.out.println(EDStatic.nTableDatasets);
+        System.out.println(EDStatic.nGridDatasets);
+        assertEquals(EDStatic.nTableDatasets - 1, 1); // -1 because Erddap makes a table listing all the datasets
+        assertEquals(EDStatic.nGridDatasets, 3);
+    }
 }
