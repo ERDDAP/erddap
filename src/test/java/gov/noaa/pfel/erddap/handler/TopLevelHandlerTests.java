c09b663dd4d8045d566b7b90dfa0deceacf6f3ea
Ayush Singh <ayushsingh01042003@gmail.com>
2024-07-06 21:53:44 +0530

    Refactor TopLevelHandlerTests for state

--- src/test/java/gov/noaa/pfel/erddap/handler/TopLevelHandlerTests.java
+++ src/test/java/gov/noaa/pfel/erddap/handler/TopLevelHandlerTests.java
@@ -1,5 +1,6 @@
 package gov.noaa.pfel.erddap.handler;
 
+import gov.noaa.pfel.erddap.handlers.SaxHandler;
 import gov.noaa.pfel.erddap.handlers.TopLevelHandler;
 import gov.noaa.pfel.erddap.util.EDStatic;
 import org.junit.jupiter.api.BeforeAll;
@@ -13,16 +14,17 @@ import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Arrays;
 import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TopLevelHandlerTests {
 
+    private static TopLevelHandler topLevelHandler;
     private static SAXParserFactory factory;
     private static SAXParser saxParser;
     private static InputStream inputStream;
+    private static SaxHandler saxHandler;
 
     @BeforeAll
     static void initAll() throws ParserConfigurationException, SAXException {
@@ -32,10 +34,13 @@ public class TopLevelHandlerTests {
         factory.setNamespaceAware(true);
         factory.setXIncludeAware(true);
         saxParser = factory.newSAXParser();
+        saxHandler = new SaxHandler();
+        saxHandler.setState(topLevelHandler);
     }
 
     @BeforeEach
     void init() {
+        topLevelHandler = new TopLevelHandler(saxHandler, new StringBuilder(), new HashMap<>());
         inputStream = TopLevelHandlerTests.class.getResourceAsStream("/datasets/topLevelHandlerTest.xml");
         if (inputStream == null) {
             throw new IllegalArgumentException("File not found: /datasets/topLevelHandlerTest.xml");
@@ -44,26 +49,28 @@ public class TopLevelHandlerTests {
 
     @Test
     void convertToPublicSourceUrlTest() throws SAXException, IOException {
-        saxParser.parse(inputStream, new TopLevelHandler(null, null));
+        saxParser.parse(inputStream, saxHandler);
         assertEquals(EDStatic.convertToPublicSourceUrl.get("http://example.com/"), "http://public.example.com/");
     }
 
     @Test
     void angularDegreeUnitsTest() throws SAXException, IOException {
-        saxParser.parse(inputStream, new TopLevelHandler(null, null));
+        saxParser.parse(inputStream, saxHandler);
         assertEquals(EDStatic.angularDegreeUnitsSet.toString(), "[angular, for, degree, units, content]");
     }
 
     @Test
     void unusualActivityTest() throws IOException, SAXException {
-        saxParser.parse(inputStream, new TopLevelHandler(null, null));
+        saxParser.parse(inputStream, saxHandler);
         assertEquals(EDStatic.unusualActivity, 25);
     }
 
     @Test
     void userTest() throws IOException, SAXException {
         var tUserHashMap = new HashMap();
-        saxParser.parse(inputStream, new TopLevelHandler(null, tUserHashMap));
+        topLevelHandler = new TopLevelHandler(saxHandler, new StringBuilder(), tUserHashMap);
+        saxHandler.setState(topLevelHandler);
+        saxParser.parse(inputStream, saxHandler);
         Object[] user1Data = (Object[]) tUserHashMap.get("user1");
         assertEquals("pass1", user1Data[0]);
     }
