3d920893da7dcbafef3b9516e169cc4b4096f36a
Ayush Singh <ayushsingh01042003@gmail.com>
2024-07-08 20:57:49 +0530

    State and SaxHandler classes

---
+++ WEB-INF/classes/gov/noaa/pfel/erddap/handlers/SaxHandler.java
@@ -0,0 +1,28 @@
+package gov.noaa.pfel.erddap.handlers;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class SaxHandler extends DefaultHandler {
+    private State state;
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        this.state.startElement(uri, localName, qName, attributes);
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        this.state.characters(ch, start, length);
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        this.state.endElement(uri, localName, qName);
+    }
+}
