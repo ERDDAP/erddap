e817b23e70036f7affa895508395928888c85ccc
Harish <peddinaharish2003@gmail.com>
2025-06-28 20:18:25 +0530

    Add JTE template dependencies, config, and initial templates (#301)
    * added dependencies,featureflags,jte_templtes
    
    * added the youarehere template and modifications
    
    * added the requested changes in the code remove the coderesolver and simplied the engine rendering as shane suggested
    
    * implemented the functionaltiy of addintrostastics in jte
    
    * more additions
    
    * added the remaining code part of status page
    
    * status page
    
    * removed commented code
    
    * Small status template changes for consistency with existing page
    
    ---------
    
    Co-authored-by: Shane St Savage <shane@axds.co>

--- WEB-INF/classes/gov/noaa/pfel/erddap/Erddap.java
+++ WEB-INF/classes/gov/noaa/pfel/erddap/Erddap.java
@@ -28,6 +28,9 @@ import com.google.common.collect.ImmutableList;
 import gg.jte.ContentType;
 import gg.jte.TemplateEngine;
 import gg.jte.output.WriterOutput;
+import gg.jte.ContentType;
+import gg.jte.TemplateEngine;
+import gg.jte.output.WriterOutput;
 import gov.noaa.pfel.coastwatch.griddata.DataHelper;
 import gov.noaa.pfel.coastwatch.griddata.Grid;
 import gov.noaa.pfel.coastwatch.griddata.OpendapHelper;
@@ -5097,7 +5100,7 @@ widgets.select("frequencyOption", "", 1, frequencyOptions, frequencyOption, "") 
               tableDatasetHashMap == null ? 0 : tableDatasetHashMap.size());
       YouAreHere youAreHere =
           EDStatic.getYouAreHere(
-              request, language, loggedInAs, EDStatic.messages.get(Message.STATUS, language));
+              request, language, loggedInAs, EDStatic.messages.statusAr[language]);
       TemplateEngine engine = TemplateEngine.createPrecompiled(ContentType.Html);
       engine.render(
           "status.jte",
@@ -5110,7 +5113,7 @@ widgets.select("frequencyOption", "", 1, frequencyOptions, frequencyOption, "") 
       try {
         String youAreHereHtml =
             EDStatic.youAreHere(
-                request, language, loggedInAs, EDStatic.messages.get(Message.STATUS, language));
+                request, language, loggedInAs, EDStatic.messages.statusAr[language]);
 
         StringBuilder sb = new StringBuilder();
         EDStatic.addIntroStatistics(sb, EDStatic.config.showLoadErrorsOnStatusPage, this);
