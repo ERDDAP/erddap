5817a972405a8d3d7a092685cbbed662264a1a89
Ayush Singh <ayushsingh01042003@gmail.com>
2025-07-10 00:24:17 +0530

    failure test cases

--- src/test/java/gov/noaa/pfel/erddap/dataset/EDDTableFromMqttTests.java
+++ src/test/java/gov/noaa/pfel/erddap/dataset/EDDTableFromMqttTests.java
@@ -82,4 +82,48 @@ class EDDTableFromMqttTests {
       String2.log("Could not connect to MQTT broker, which is acceptable for this test.");
     }
   }
+
+  @org.junit.jupiter.api.Test
+  void testProcessMqttData_fewerColumns() throws Throwable {
+    EDDTableFromMqtt eddTableFromMqtt = (EDDTableFromMqtt) EDDTestDataset.gettestFromMqtt();
+    eddTableFromMqtt.fileDir = TEMP_DIR.toAbsolutePath().toString();
+    Mqtt5Publish publish = Mockito.mock(Mqtt5Publish.class);
+    String topic = "sensor/data_fewer";
+    String payload = "{\"lat\": 20.0, \"lon\": -150.0}";
+    Mockito.when(publish.getTopic())
+        .thenReturn(com.hivemq.client.mqtt.datatypes.MqttTopic.of(topic));
+    Mockito.when(publish.getPayloadAsBytes()).thenReturn(payload.getBytes());
+    eddTableFromMqtt.processMqttData(publish);
+
+    String expectedFilePath = eddTableFromMqtt.getFilePathForTopic(topic);
+    Table resultTable = new Table();
+    resultTable.readJsonlCSV(expectedFilePath, null, null, true);
+    Test.ensureEqual(resultTable.nRows(), 1, "nRows");
+    Test.ensureEqual(resultTable.getFloatData(0, 0), 20.0f, "lat");
+    Test.ensureEqual(resultTable.getFloatData(1, 0), -150.0f, "lon");
+    Test.ensureTrue(Float.isNaN(resultTable.getFloatData(2, 0)), "temperature is NaN");
+  }
+
+  @org.junit.jupiter.api.Test
+  void testProcessMqttData_moreColumns() throws Throwable {
+    EDDTableFromMqtt eddTableFromMqtt = (EDDTableFromMqtt) EDDTestDataset.gettestFromMqtt();
+    eddTableFromMqtt.fileDir = TEMP_DIR.toAbsolutePath().toString();
+    Mqtt5Publish publish = Mockito.mock(Mqtt5Publish.class);
+    String topic = "sensor/data_more";
+    String payload =
+        "{\"lat\": 20.0, \"lon\": -150.0, \"temperature\": 22.5, \"extra_col\": \"dummy\"}";
+    Mockito.when(publish.getTopic())
+        .thenReturn(com.hivemq.client.mqtt.datatypes.MqttTopic.of(topic));
+    Mockito.when(publish.getPayloadAsBytes()).thenReturn(payload.getBytes());
+    eddTableFromMqtt.processMqttData(publish);
+
+    String expectedFilePath = eddTableFromMqtt.getFilePathForTopic(topic);
+    Table resultTable = new Table();
+    resultTable.readJsonlCSV(expectedFilePath, null, null, true);
+    Test.ensureEqual(resultTable.nRows(), 1, "nRows");
+    Test.ensureEqual(resultTable.nColumns(), 3, "nColumns");
+    Test.ensureEqual(resultTable.getFloatData(0, 0), 20.0f, "lat");
+    Test.ensureEqual(resultTable.getFloatData(1, 0), -150.0f, "lon");
+    Test.ensureEqual(resultTable.getFloatData(2, 0), 22.5f, "temperature");
+  }
 }
