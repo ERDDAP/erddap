67d4b3bc9d06fbb68414ff8e91b95148b1c39777
Chris John <chris.john@noaa.gov>
2025-07-15 14:43:41 -0400

    Add workflow to generate docker image and push on update of the main branch.

--- WEB-INF/classes/gov/noaa/pfel/erddap/util/EDStatic.java
+++ WEB-INF/classes/gov/noaa/pfel/erddap/util/EDStatic.java
@@ -200,7 +200,7 @@ public class EDStatic {
    * anything following it. A request to http.../erddap/version will return just the number (as
    * text). A request to http.../erddap/version_string will return the full string.
    */
-  public static final Semver erddapVersion = new Semver("2.27.0");
+  public static final Semver erddapVersion = new Semver("2.28.0-alpha");
 
   /** This identifies the dods server/version that this mimics. */
   public static final String dapVersion = "DAP/2.0";
