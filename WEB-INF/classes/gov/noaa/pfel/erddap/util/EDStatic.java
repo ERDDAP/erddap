ea0a337434dbb40a2f9f0bea58c1d7e8afc9c4a5
Chris John <chris.john@noaa.gov>
2025-08-01 13:16:20 -0400

    Update the Legal template to use the new getMessage

--- WEB-INF/classes/gov/noaa/pfel/erddap/util/EDStatic.java
+++ WEB-INF/classes/gov/noaa/pfel/erddap/util/EDStatic.java
@@ -1122,7 +1122,7 @@ public class EDStatic {
       HttpServletRequest request, int language, String loggedInAs, String protocol) {
     return new YouAreHere(
         erddapUrl(request, loggedInAs, language),
-        EDStatic.messages.clickERDDAPAr[language],
+        EDStatic.messages.get(Message.CLICK_ERDDAP, language),
         protocol);
   }
 
